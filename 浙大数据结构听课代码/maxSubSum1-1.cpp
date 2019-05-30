f589bdb60248b5ff631f768b128c8931f4492511
ISwuguangli <3317415537@qq.com>
2019-05-30 21:28:56 +0800

    最大子列和在线处理

--- 浙大数据结构听课代码/maxSubSum1-1.cpp
+++ 浙大数据结构听课代码/maxSubSum1-1.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
-//提供一个错误例子，已改正 
+//正解 
 int main(){
 	long now = 0;
 	long max = 0;
-	long pr = 0;
 	long a[100000];
 	long n;
 	cin>>n;
@@ -13,14 +12,11 @@ int main(){
 	}
 	for(int i=0;i<n;i++){
 		now+=a[i];
-		if(now < pr){
-			//这里可以改的now<max，pr就是maxs 
-			//不是所有都要舍弃，小于零舍弃 
-			now = 0;
+		if(now > max){
+			max = now;
 		}
-		if(now>=pr){
-			pr = now;
-			max=now;
+		if(now < 0){
+			now = 0;
 		}
 	}
 	cout<<max;
--- 浙大数据结构听课代码/maxSubSum2.cpp
+++ 浙大数据结构听课代码/maxSubSum2.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
 using namespace std;
-//提供一个错误例子，已改正 
+//找出第一个元素和最后一个元素的数 
 int main(){
 	long now = 0;
 	long max = 0;
-	long pr = 0;
 	long a[100000];
 	long n;
+	int len = 0;
+	int num = 0; 
 	cin>>n;
 	for(int i=0;i<n;i++){
 		cin>>a[i];
 	}
 	for(int i=0;i<n;i++){
 		now+=a[i];
-		if(now < pr){
-			//这里可以改的now<max，pr就是maxs 
-			//不是所有都要舍弃，小于零舍弃 
-			now = 0;
+		len++;
+		if(now > max){
+			max = now;
+			num = i;
 		}
-		if(now>=pr){
-			pr = now;
-			max=now;
+		if(now < 0){
+			now = 0;
+			len = 0;
 		}
 	}
-	cout<<max;
+	cout<<max<<" "<<a[num - len+1]<<" "<<a[num];
 	return 0;
 } 
 
