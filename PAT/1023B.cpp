12c927d927cfeea86ac532045e3677b68be8f45e
ISwuguangli <3317415537@qq.com>
2019-05-30 21:24:36 +0800

    PAT B1023

---
+++ PAT/1023B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a[9];
+	for(int i=0;i<10;i++){
+		cin>>a[i];
+	}
+	for(int i=1;i<10;i++){
+		if(a[i]!=0){
+			cout<<i;
+			a[i]--;
+		}
+		break;
+	}
+	for(int i=0;i<10;i++){
+		while(a[i]!=0){
+			cout<<i;
+			a[i]--;
+		}
+	}
+	return 0;
+}
